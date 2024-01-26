cc2f95cb137fb372c8447e2d3ab7ee5781bcafb6
Nabaneet <dutta.navneet17@gmail.com>
2024-01-26 11:16:59 +0530

    Added doctor and speciality

--- medico/backend/src/main/java/com/medico/app/entities/Speciality.java
+++ medico/backend/src/main/java/com/medico/app/entities/Speciality.java
@@ -3,7 +3,7 @@ package com.medico.app.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,21 +11,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "doctors")
+@Table(name = "speciality")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Doctor {
+public class Speciality {
 
     @Id
-    @Column(name = "id")
-    private Integer docId;
+    @Column()
+    private Integer specialityId;
 
-    @Column(name = "doctor_name")
-    private String docName;
-
-    @Column(name = "doctor_speciality")
-    @ManyToOne()
-    private Speciality speciality;
+    @Column()
+    @OneToMany
+    private String specialityName;
 }
