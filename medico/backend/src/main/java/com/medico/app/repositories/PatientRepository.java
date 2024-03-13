2580517a3175fa0e8039de4ab7807e19bfc11c93
Tezaswa06 <tezaswaawasthi@gmail.com>
2024-03-13 19:54:24 +0530

    book consultation api

---
+++ medico/backend/src/main/java/com/medico/app/repositories/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.medico.app.repositories;
+
+import com.medico.app.entities.Patient;
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
