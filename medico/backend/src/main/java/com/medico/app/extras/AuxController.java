41510027faaa57ccf94a02e2769901c649c8cf7c
navneet171996 <dutta.navneet17@gmail.com>
2024-05-06 21:40:54 +0530

    Added mail APIs

--- medico/backend/src/main/java/com/medico/app/extras/AuxController.java
+++ medico/backend/src/main/java/com/medico/app/extras/AuxController.java
@@ -1,12 +1,10 @@
 package com.medico.app.extras;
 
 import com.medico.app.extras.dto.StartupDto;
+import com.medico.app.services.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +13,11 @@ import java.util.List;
 public class AuxController {
 
     private final AuxService auxService;
+    private final EmailService emailService;
 
-    public AuxController(AuxService auxService) {
+    public AuxController(AuxService auxService, EmailService emailService) {
         this.auxService = auxService;
+        this.emailService = emailService;
     }
 
 
@@ -25,6 +25,11 @@ public class AuxController {
     public ResponseEntity<?> startup(@RequestBody StartupDto startupDto){
         this.auxService.startup(startupDto);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/sendMail")
+    public ResponseEntity<?> sendMail(){
+        return new ResponseEntity<>(emailService.sendEmail("sumitpaliya9087@gmail.com", "Test", "Tezas Bhadwa"), HttpStatus.OK);
     }
 
 }
